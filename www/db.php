8e96e4020ccd734190da9c6a0be095d1bb40a017
Arinamanasikana <48472347+Arinamanasikana@users.noreply.github.com>
2019-05-20 01:35:04 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8951444_data","12345678","id8951444_data") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id8951560_cordova","1905ktbffh","id8951560_peminjaman") or die ("could not connect database");
 ?>
