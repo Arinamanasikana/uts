43a97f436fb75d2f35cdc07b08cb5c482e57616a
Arinamanasikana <48472347+Arinamanasikana@users.noreply.github.com>
2019-07-30 13:22:15 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -4,9 +4,12 @@
  {
  $Id=$_POST['Id'];
  $nama=$_POST['nama'];
+ $alamat=$_POST['alamat'];
+ $asal_sekolah=$_POST['asal_sekolah'];
+ $jenis_kelamin=$_POST['jenis_kelamin'];
  $judul_buku=$_POST['judul_buku'];
  $penulis=$_POST['penulis'];
- $q=mysqli_query($con,"UPDATE `data` SET `nama`='$nama',`judul_buku`='$judul_buku',`penulis`='$penulis' where `Id`='$Id'");
+ $q=mysqli_query($con,"UPDATE `data` SET `nama`='$nama',`alamat`='$alamat',`asal_sekolah`='$asal_sekolah',`jenis_kelamin`='$jenis_kelamin',`judul_buku`='$judul_buku',`penulis`='$penulis' where `Id`='$Id'");
  if($q)
  echo "success";
  else
