b522a87b3ebf1e4153ef1fb55150935529663fe8
Arinamanasikana <48472347+Arinamanasikana@users.noreply.github.com>
2019-07-30 13:20:07 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -3,9 +3,12 @@
  if(isset($_POST['insert']))
  {
  $nama=$_POST['nama'];
+ $alamat=$_POST['alamat'];
+ $asal_sekolah=$_POST['asal_sekolah'];
+ $jenis_kelamin=$_POST['jenis_kelamin'];
  $judul_buku=$_POST['judul_buku'];
  $penulis=$_POST['penulis'];
- $q=mysqli_query($con,"INSERT INTO `data` (`nama`,`judul_buku`,`penulis`) VALUES ('$nama','$judul_buku','$penulis')");
+ $q=mysqli_query($con,"INSERT INTO `data` (`nama`,`alamat`,`asal_sekolah`,`jenis_kelamin`,`judul_buku`,`penulis`) VALUES ('$nama','$alamat','$asal_sekolah','$jenis_kelamin','$judul_buku','$penulis')");
  if($q)
   echo "success";
  else
